2ac52adfc128364a237177b29fd149446a22ed71
aiym182 <aiym182@gmail.com>
2022-08-13 02:34:36 -0400

    scroll and modal for Eng

--- static/javascript/modal.js
+++ static/javascript/modal.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function closeModal($el) {
+    closeScroll()
     $el.classList.remove('is-active')
   }
 
@@ -49,3 +50,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 })
+
+function lockScroll() {
+  var scrollPosition = [
+    self.pageXOffset ||
+      document.documentElement.scrollLeft ||
+      document.body.scrollLeft,
+    self.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop,
+  ]
+  var html = jQuery('html') // it would make more sense to apply this to body, but IE7 won't have that
+  html.data('scroll-position', scrollPosition)
+  html.data('previous-overflow', html.css('overflow'))
+  html.css('overflow', 'hidden')
+  window.scrollTo(scrollPosition[0], scrollPosition[1])
+}
+
+function closeScroll() {
+  var html = jQuery('html')
+  var scrollPosition = html.data('scroll-position')
+  html.css('overflow', html.data('previous-overflow'))
+  window.scrollTo(scrollPosition[0], scrollPosition[1])
+}
